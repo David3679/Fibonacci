312bc4cd27c8d2d3291b0c77f831b1eea6e9f20c
david <david@DESKTOP-03H3UL1>
2023-04-25 09:22:05 -0300

    Adicionar arquivos de projeto.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um numero do Fibonacci para o calculo:");
+        int num = int.Parse(Console.ReadLine());
+        int a = 0, b = 1, c = 0;
+        bool encontrado = false;
+
+        while (c < num)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+            Console.WriteLine(c);
+        }
+
+        if (c == num)
+        {
+            encontrado = true;
+        }
+
+        if (encontrado)
+        {
+            Console.WriteLine($"{num} pertence à sequência de Fibonacci.");
+        }
+        else
+        {
+            Console.WriteLine($"{num} não pertence à sequência de Fibonacci.");
+        }
+    }
+}
